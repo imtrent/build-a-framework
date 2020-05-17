19c7921f47679449123eb671f57b23163e06fc37
Ian Trent <ian.m.trent@gmail.com>
2020-05-17 09:25:42 -0400

    Add ability to toggle the tabs with a dropdown on mobile

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,21 +2,51 @@ import React from 'react';
 
 const Tabs = ({ items, selected, handleChange }) => {
 	return (
-		<nav className="-ml-4 mb-8">
-			{items.map((item, index) => {
-				return (
-					<button
-						key={index}
-						onClick={() => handleChange(item)}
-						className={`${
-							selected === item ? 'bg-gray-800 text-white' : ' text-gray-800'
-						} ml-4 px-3 py-2 bg-gray-200 font-medium text-sm leading-5 rounded-md focus:outline-none`}
+		<div>
+			<div className="sm:hidden">
+				<label htmlFor="component" className="block text-gray-700 font-medium mb-2">
+					Select Component Type
+				</label>
+				<div className="relative">
+					<select
+						onChange={(evt) => handleChange(evt.target.value)}
+						id="component"
+						ariaLabel="Selected tab"
+						className="block mb-6 appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none"
 					>
-						{item}
-					</button>
-				);
-			})}
-		</nav>
+						{items.map((item, index) => {
+							return (
+								<option key={index} selected={selected === item}>
+									{item}
+								</option>
+							);
+						})}
+					</select>
+					<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+						<svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+							<path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+						</svg>
+					</div>
+				</div>
+			</div>
+			<div className="hidden sm:block">
+				<nav className="-ml-4 mb-8">
+					{items.map((item, index) => {
+						return (
+							<button
+								key={index}
+								onClick={() => handleChange(item)}
+								className={`${
+									selected === item ? 'bg-gray-800 text-white' : ' text-gray-700'
+								} ml-4 px-3 py-2 bg-gray-200 font-medium text-sm leading-5 rounded-md focus:outline-none`}
+							>
+								{item}
+							</button>
+						);
+					})}
+				</nav>
+			</div>
+		</div>
 	);
 };
 
